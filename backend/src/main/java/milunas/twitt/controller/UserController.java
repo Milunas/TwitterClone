351d033c9b22254d73bbd64e74854f42521172bc
Łukasz Milunas <milunas.lukasz@gmail.com>
2018-04-27 12:10:40 +0200

    change usercontroller name

--- backend/src/main/java/milunas/twitt/controller/UserController.java
+++ backend/src/main/java/milunas/twitt/controller/UserController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
-public class TwittController {
+public class UserController {
 
     @Autowired
     private UserService service;
